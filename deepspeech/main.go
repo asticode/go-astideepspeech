18c72564787dc1ae492986c91d4ca6e9b216aacf
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2019-01-09 15:02:03 +0100

    Update LM parameters
    Fixes #4

--- deepspeech/main.go
+++ deepspeech/main.go
@@ -17,8 +17,8 @@ const (
 	beamWidth            = 500
 	nCep                 = 26
 	nContext             = 9
-	lmWeight             = 1.50
-	validWordCountWeight = 2.10
+	lmWeight             = 0.75
+	validWordCountWeight = 1.85
 )
 
 var model    = flag.String("model", "",    "Path to the model (protocol buffer binary file)")
